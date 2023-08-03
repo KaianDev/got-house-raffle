1aae0f5eb7d32a69d0071d238c4b2e81ce7ba237
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-08-03 15:15:48 -0300

    Ajuste do botão de reiniciar

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const RestartButton = ({ onClick }: Props) => {
     return (
-        <div className="flex mt-10 justify-center items-center">
+        <div className="flex mt-4 sm:mt-10 justify-center items-center">
             <button
                 className="px-5 py-3 text-lg font-bold bg-[#9b7c01] text-slate-100 rounded-md tracking-wide transition duration-300 hover:opacity-90"
                 onClick={onClick}
